9231fe63107e67963c349db60466ca1ca146757b
YousifYassin <yousifaliyassin@gmail.com>
2025-01-28 15:41:59 +0200

    finish colors section

--- src/css_views/colors.nim
+++ src/css_views/colors.nim
@@ -3,7 +3,9 @@ import nimja/parser
 
 proc colors_fn*(ctx: Context) {.async.} =
   proc render_color_page(): string =
-    let title = "Colors"
+    let
+      title = "Colors"
+      numbers = (1..3)
     compileTemplateFile(path = "../css_templates/04_colors.html", baseDir = getScriptDir())
 
   resp render_color_page()
